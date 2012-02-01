9e7e3e5bfe5567d76083100696795ec33ca63afb
adampickeral <adam.pickeral@rackspace.com>
2012-02-01 15:23:19 -0500

    Added views for picking and displaying the winner of the challenge

--- fitness_challenge/entry_system/urls.py
+++ fitness_challenge/entry_system/urls.py
@@ -4,4 +4,7 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('entry_system.views',
     url(r'^$', 'entry'),
     url(r'^process/', 'process'),
+    url(r'^drawing/', 'drawing'),
+    url(r'^pick_winner/', 'pick_winner'),
+    url(r'^winner/(?P<entry_id>\d+)', 'winner'),
 )
--- fitness_challenge/entry_system/views.py
+++ fitness_challenge/entry_system/views.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template.context import RequestContext
 from fitness_challenge.entry_system.models import WorkoutEntry
 
@@ -9,7 +9,22 @@ def entry(request):
 
 
 def process(request):
-    print(request.POST)
     entry = WorkoutEntry(name=request.POST['name'], date=request.POST['date'])
     entry.save()
     return HttpResponseRedirect(reverse('entry_system.views.entry'))
+
+
+def drawing(request):
+    return render_to_response("entry/drawing.html", context_instance=RequestContext(request))
+
+
+def pick_winner(request):
+    entries = WorkoutEntry.objects.all()
+
+    return HttpResponseRedirect(reverse('entry_system.views.winner', args=(entries[2].id,)))
+
+
+def winner(request, entry_id):
+    entry = WorkoutEntry.objects.get(id=entry_id)
+
+    return render_to_response("entry/winner.html", {'entry': entry})
